45695721f11609fe2e73e0b1c1369c16784863de
schuhe <schuhe@gmail.com>
2026-03-15 18:39:08 -0400

    fix: relax CSP to allow Toast UI Editor WYSIWYG editing (unsafe-eval, img-src, data: fonts)

--- src/views/editor-panel.ts
+++ src/views/editor-panel.ts
@@ -110,7 +110,7 @@ export class EditorPanelManager {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}'; font-src ${webview.cspSource};">
+  <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}' 'unsafe-eval'; font-src ${webview.cspSource} data:; img-src ${webview.cspSource} data: https:;">
   <link href="${editorCssUri}" rel="stylesheet">
   <style>
     :root {
